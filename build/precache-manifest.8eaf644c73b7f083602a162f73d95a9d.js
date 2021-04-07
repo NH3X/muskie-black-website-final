520befe99a298679a0c00aa638443a4b8f34d5a0
philnhex3 <37580308+philnhex3@users.noreply.github.com>
2021-04-07 10:10:11 +0200

    Build version added

---
+++ build/precache-manifest.8eaf644c73b7f083602a162f73d95a9d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3523671e001006dba785c0ea0d03040f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6824fd0091422a2c2ee",
+    "url": "/static/css/main.e1d4b855.chunk.css"
+  },
+  {
+    "revision": "5e32036d82ffe928d301",
+    "url": "/static/js/2.feb0f927.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/static/js/2.feb0f927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6824fd0091422a2c2ee",
+    "url": "/static/js/main.b6293ba6.chunk.js"
+  },
+  {
+    "revision": "e0bdb04da260028d8073",
+    "url": "/static/js/runtime-main.c1f1df22.js"
+  },
+  {
+    "revision": "3dc6eb74a8675d947082460645072bda",
+    "url": "/static/media/bg.3dc6eb74.jpg"
+  },
+  {
+    "revision": "b7340a701ce030215d32627a4d4542fb",
+    "url": "/static/media/home-img.b7340a70.png"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "386b5bb638a06bbb6a9899b5251e0754",
+    "url": "/static/media/muskie_bg.386b5bb6.png"
+  },
+  {
+    "revision": "01b23533608c71166a26e8527bd02b69",
+    "url": "/static/media/muskie_bg2.01b23533.jpg"
+  },
+  {
+    "revision": "030be37b836968b02d06c98eb66609fe",
+    "url": "/static/media/muskie_bg3.030be37b.jpg"
+  },
+  {
+    "revision": "2951d0095f54f8e5fe9e20b0fc95127b",
+    "url": "/static/media/muskie_bg4.2951d009.jpg"
+  }
+]);
