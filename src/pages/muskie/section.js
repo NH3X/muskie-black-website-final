93f33e5bdf613c668f35d0d7ebdf78bc9e4de948
philnhex3 <37580308+philnhex3@users.noreply.github.com>
2021-05-15 21:26:56 +0200

    Hero title more responsive

--- src/pages/muskie/section.js
+++ src/pages/muskie/section.js
@@ -22,30 +22,66 @@ class   Section extends Component {
 
     render() {
         return (
-            <React.Fragment>
-                <section className="hero-section-5" id="home">
-                    <div className="bg-overlay"></div>
-                    <Container>
-                        <Row className="justify-content-center">
-                            <Col lg={8} className="align-content-center">
-                                <div className="hero-wrapper text-center" >
-                                    {/* <p className="font-16 text-uppercase text-white-50">Discover Muskie Black Today</p> */}
-                                    <h1 className="hero-title text-white mb-5" style={{fontFamily: "Amsterdam", fontSize:"7em"}}>Muskie Black<br /></h1>
-                                    <h1 className="hero-title text-white mb-5" ><span className="text-primary">Management & Consulting</span><br /></h1>
+			<React.Fragment>
+				<section className="hero-section-5" id="home">
+					<div className="bg-overlay"></div>
+					<Container>
+						<Row className="justify-content-center">
+							<Col lg={8} className="align-content-center">
+								<div className="hero-wrapper text-center">
+									{/* <p className="font-16 text-uppercase text-white-50">Discover Muskie Black Today</p> */}
+									<div className="text-responsive">
+										<h1
+											className="text-responsive hero-title text-white mb-5 mt-0"
+											style={{
+												fontFamily: "Amsterdam",
+												fontSize: "calc(110% + 4vw + 2vh)",
+											}}
+										>
+											Muskie Black
+											<br />
+										</h1>
+									</div>
+									<h1
+										className="hero-title text-white mb-5"
+										style={{
+											fontSize: "2em",
+										}}
+									>
+										<span className="text-primary">
+											Management & Consulting
+										</span>
+										<br />
+									</h1>
 
-                                    <p className='font-20 text-white'>Assisting your business virtually. Wherever. Whenever.</p>
+									<p
+										className="font-20 text-white"
+										style={{
+											fontSize: "1.5em",
+										}}
+									>
+										Assisting your business virtually.
+										Wherever. Whenever.
+									</p>
 
-                                    <div className="mt-4">
-                                        <Button onClick={() => this.props.scrollToContact()} className="btn btn-primary mt-2 mr-2">Enquire Now</Button>
-                                        {/* <Link to="#" className="btn btn-success mt-2 mr-2 ml-1">Learn more</Link> */}
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Container>
-                </section>
-            </React.Fragment>
-        );
+									<div className="mt-4">
+										<Button
+											onClick={() =>
+												this.props.scrollToContact()
+											}
+											className="btn btn-primary mt-2 mr-2"
+										>
+											Enquire Now
+										</Button>
+										{/* <Link to="#" className="btn btn-success mt-2 mr-2 ml-1">Learn more</Link> */}
+									</div>
+								</div>
+							</Col>
+						</Row>
+					</Container>
+				</section>
+			</React.Fragment>
+		)
     }
 }
 
