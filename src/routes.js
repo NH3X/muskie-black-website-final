677f454a55e4fa2146ece77b800387af3fdfc54b
philnhex3 <37580308+philnhex3@users.noreply.github.com>
2021-08-25 13:59:09 +0200

    cpanel file added

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
+import privacy from "./components/PrivacyPolicy/privacy";
 import Muskie from "./pages/muskie/Index1";
 
 const routes = [
   { path: "/", component: Muskie },
+  { path: '/privacypolicy', component: privacy, }
 ];
 
 export default routes;
